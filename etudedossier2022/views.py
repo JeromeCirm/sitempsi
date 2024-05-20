72168254a32fb2a077a8025279c3dcdab8c11e9a
JeromeCirm <jerome.nizon@gmail.com>
2024-05-20 23:16:00 +0200

    .

--- etudedossier2022/views.py
+++ etudedossier2022/views.py
@@ -33,8 +33,10 @@ def traitement(request):
     if request.method=='POST':
         if "extraction_donnees" in request.POST and request.POST["extraction_donnees"]=="oui":
             context["extraction_msg"]=extraction_donnees(request)
-        elif "NoteAChanger" in request.POST and request.POST["NoteAChanger"]=="true":
-            maj_dossier(request)        
+        else:
+            return # empeche les modification de notes
+        #elif "NoteAChanger" in request.POST and request.POST["NoteAChanger"]=="true":
+        #    maj_dossier(request)                   
     if lire_un_dossier(request,context):
         return render(request,'etudedossier2022/traitement.html',context)
     else:
